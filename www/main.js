0fd4f5ab8e1e39f1e455c010e95816cb7c255d01
Joel Diogo Marques Ferreira <joelferreira.dh@gmail.com>
2013-07-17 23:36:31 +0100

    teste 1
    deviceReady directo

--- www/main.js
+++ www/main.js
@@ -1,11 +1,15 @@
 function init() {
+    alert("init");
+    this.deviceReady();
 	document.addEventListener("deviceready", deviceReady, true);
 	delete init;
 }
 
 function checkPreAuth() {
+    alert("checkPreAuth");
 	console.log("checkPreAuth");
     var form = $("#loginForm");
+    alert("passou");
     if(window.localStorage["username"] != undefined && window.localStorage["password"] != undefined) {
         $("#username", form).val(window.localStorage["username"]);
         $("#password", form).val(window.localStorage["password"]);
@@ -14,6 +18,7 @@ function checkPreAuth() {
 }
 
 function handleLogin() {
+    alert("handleLogin");
     var form = $("#loginForm");    
     //disable the button so we can't resubmit while we wait
     $("#submitButton",form).attr("disabled","disabled");
@@ -21,6 +26,7 @@ function handleLogin() {
     var p = $("#password", form).val();
     if(u != '' && p!= '') {
         $.post("http://www.coldfusionjedi.com/demos/2011/nov/10/service.cfc?method=login&returnformat=json", {username:u,password:p}, function(res) {
+            alert(res);
             if(res == true) {
                 //store
                 window.localStorage["username"] = u;
@@ -39,6 +45,7 @@ function handleLogin() {
 }
 
 function deviceReady() {
+    alert("deviceReady");
 	console.log("deviceReady");
 	$("#loginPage").on("pageinit",function() {
 		console.log("pageinit run");
